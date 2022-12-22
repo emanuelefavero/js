b55d26b2aa3b42d5b589a267e5cd4dd2469e16b7
emanuelefavero <emafaux89@gmail.com>
2022-12-22 03:18:35 +0100

    add exponentiation operator **

---
+++ exponentiation-operator.js
@@ -0,0 +1,9 @@
+// pow (exponentiation operator)
+// The exponentiation operator (**) raises the first operand to the power of the second operand.
+const pow = (x, y) => x ** y
+
+console.log(pow(2, 3)) // 2 * 1 = 2, 2 * 2 = 4, 4 * 2 = 8
+console.log(pow(2, 4)) // 2 * 1 = 2, 2 * 2 = 4, 4 * 2 = 8, 8 * 2 = 16
+
+console.log(2 ** 3) // 2 * 1 = 2, 2 * 2 = 4, 4 * 2 = 8
+console.log(2 ** 4) // 2 * 1 = 2, 2 * 2 = 4, 4 * 2 = 8, 8 * 2 = 16
