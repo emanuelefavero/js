a21c1dd2263aa4489b81e2da3b5aca6839b99290
emanuelefavero <emafaux89@gmail.com>
2023-01-15 23:58:44 +0100

    add array-indexOf.js

---
+++ array-indexOf.js
@@ -0,0 +1,4 @@
+// ARRAY INDEXOF - Returns the first index at which a given element can be found in the array, or -1 if it is not present
+let ages = [18, 18, 21, 34]
+console.log(ages.indexOf(18)) // 0
+console.log(ages.indexOf(33)) // -1
