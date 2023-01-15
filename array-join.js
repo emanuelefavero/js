b62a6fdfba8f7c8d1ea1e84079dd621584a38004
emanuelefavero <emafaux89@gmail.com>
2023-01-16 00:03:33 +0100

    add array-join.js

---
+++ array-join.js
@@ -0,0 +1,5 @@
+// ARRAY JOIN - Joins all elements of an array into a string
+let ages = [18, 21, 34]
+console.log(ages.join()) // 18,21,34
+console.log(ages.join(' ')) // 18 21 34
+console.log(ages.join('')) // 182134
