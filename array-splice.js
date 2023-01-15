fd753fa30bcc4a50b34148cd2a5733fd6aaad50a
emanuelefavero <emafaux89@gmail.com>
2023-01-15 23:28:03 +0100

    add array-splice.js

---
+++ array-splice.js
@@ -0,0 +1,18 @@
+let fruits = ['banana', 'cherry', 'kiwi', 'orange']
+console.log('original', fruits)
+
+// SPLICE - replace or remove elements in array
+// removes item, returns that item [ 'banana' ]
+console.log('splice(0, 1)', fruits.splice(0, 1))
+
+console.log('original', fruits)
+
+// removes items from index 1 to end, returns removed items [ 'kiwi', 'orange' ]
+console.log('splice(1)', fruits.splice(1))
+
+console.log('original', fruits)
+
+// replace items from index 0 to 1, returns removed items [ 'cherry' ]
+console.log('splice(0, 1, "apple")', fruits.splice(0, 1, 'apple'))
+
+console.log('original', fruits)
