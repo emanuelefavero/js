5c83feb1c226071baf63ac519c7bf44ce7a41ba4
emanuelefavero <emafaux89@gmail.com>
2023-01-15 23:49:50 +0100

    add array-every.js

---
+++ array-every.js
@@ -0,0 +1,4 @@
+// ARRAY EVERY - Returns true if all elements in an array pass a test
+let ages = [18, 21, 34]
+console.log(ages.every((age) => age >= 18)) // true
+console.log(ages.every((age) => age >= 21)) // false
