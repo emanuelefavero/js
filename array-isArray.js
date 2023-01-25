5ad50ab2ed4823ee916eccaa998399f27340bb3f
emanuelefavero <emafaux89@gmail.com>
2023-01-25 15:42:02 +0100

    add array-isArray.js

---
+++ array-isArray.js
@@ -0,0 +1,7 @@
+// Array.isArray() returns true if the argument is an array, and false if it is not
+
+const nums = [1, 2, 3]
+const text = 'Hello'
+
+console.log(Array.isArray(nums)) // true
+console.log(Array.isArray(text)) // false
