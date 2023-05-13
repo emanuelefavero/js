30007db7e881e769be358050694e3b7428d66657
emanuelefavero <emafaux89@gmail.com>
2023-05-14 01:29:48 +0200

    add array-shift

---
+++ array-shift.js
@@ -0,0 +1,7 @@
+// Array Shift - removes the first element from an array
+const fruits = ['banana', 'cherry', 'kiwi']
+fruits.shift()
+
+console.log(fruits) // [ 'cherry', 'kiwi' ]
+
+console.log(fruits.shift()) // cherry - returns the removed element
