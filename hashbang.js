db698d6c30b9fe678ee662430c67f2ed8e5d38d1
emanuelefavero <emafaux89@gmail.com>
2023-05-15 07:37:10 +0200

    add hashbang

---
+++ hashbang.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+console.log(2)
+
+// es2023 new feature: hashbang
+// TIP: The hashbang is commonly used to indicate the interpreter or command needed to execute the script in unix systems. es2023 adds support for hashbang
+// TIP: The hashbang indicates a comment that can contain any text
+// NOTE: The hashbang can only be used in the first line of the file
