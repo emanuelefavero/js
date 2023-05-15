48830cac38911e6657f75e15450b99bcc614d71a
emanuelefavero <emafaux89@gmail.com>
2023-05-15 02:04:00 +0200

    add array-some

---
+++ array-some.js
@@ -0,0 +1,4 @@
+// Array Some - returns true if at least one element in the array passes the test implemented by the provided function
+const ages = [3, 10, 18, 20]
+
+console.log(ages.some((age) => age >= 18)) // true
