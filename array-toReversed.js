3a01c12d2b91c45b6056fd281ed8fa9f8d903b12
emanuelefavero <emafaux89@gmail.com>
2023-05-15 08:07:16 +0200

    udpate array-toReversed

--- array-toReversed.js
+++ array-toReversed.js
@@ -1,7 +1,7 @@
 // ! NOTE: Need node v20.1.0 or higher (use nvmrc to switch to this version if you want to test this feature)
 
 // es2023 new feature:
-// toReversed - returns a sorted array without changing the original array
+// toReversed - returns a reversed array without changing the original array
 const nums = [3, 1, 2]
 console.log(nums.toReversed()) // [2, 1, 3]
 console.log(nums) // [3, 1, 2]
