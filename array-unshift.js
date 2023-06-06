42ed8eae9a48f582825a834678b6dd2994566c0a
emanuelefavero <emafaux89@gmail.com>
2023-06-06 18:04:58 +0200

    add array-unshift

---
+++ array-unshift.js
@@ -0,0 +1,7 @@
+// unshift - add new elements to the beginning of an array
+const fruits = ['banana', 'cherry', 'kiwi']
+
+// This method changes the original array and returns the new length
+console.log(fruits.unshift('apple', 'orange')) // 5
+
+console.log(fruits) // [ 'apple', 'orange', 'banana', 'cherry', 'kiwi' ]
