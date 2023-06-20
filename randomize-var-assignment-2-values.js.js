c4312e4ab474e725be1c6753edb2d696f7748c75
emanuelefavero <emafaux89@gmail.com>
2023-06-20 10:36:04 +0200

    add randomize-var-assignment-2-values

---
+++ randomize-var-assignment-2-values.js.js
@@ -0,0 +1,6 @@
+// Randomize variable assignment between 2 values
+let upOrDown = Math.random() < 0.5 ? 'up' : 'down'
+
+console.log(upOrDown)
+
+// TIP: Run the code multiple times to see the result
