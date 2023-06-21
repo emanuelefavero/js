d3892625af41ab6bc6cb89ef9176f7c2a36d13d4
emanuelefavero <emafaux89@gmail.com>
2023-06-21 03:42:26 +0200

    add charCodeAt

---
+++ charCodeAt.js
@@ -0,0 +1,8 @@
+// charCodeAt - returns the unicode of the character at the specified index in a string
+
+console.log('!'.charCodeAt(0)) // 33
+console.log('A'.charCodeAt(0)) // 65
+console.log('a'.charCodeAt(0)) // 97
+console.log('€'.charCodeAt(0)) // 8364
+
+// TIP: UNICODE is a standard that assigns a unique number to every character in every language
