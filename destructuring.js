3d0efefe0da4b762fef32717a7596de127a52ce6
emanuelefavero <emafaux89@gmail.com>
2023-06-22 20:11:15 +0200

    add object destructuring

--- destructuring.js
+++ destructuring.js
@@ -16,3 +16,26 @@ console.log(e, f) // hello ciao
 let array4 = ['hello', 'ciao', 'hola', 'hallo']
 let [g, ...h] = array4
 console.log(g, h) // hello [ 'ciao', 'hola', 'hallo' ]
+
+// Object Destructuring
+let object = {
+  firstName: 'John',
+  age: 34,
+}
+let { firstName, age } = object
+console.log(firstName, age) // John 34
+
+let object2 = {
+  lastName: 'Doe',
+}
+let { lastName = 'White', surName = 'White' } = object2
+console.log(lastName, surName) // Doe White (surName key does not exist so the default will be displayed)
+
+let colors = {
+  red: '#f00',
+  green: '#0f0',
+  blue: '#00f',
+}
+let { red, ...otherColors } = colors
+console.log(red, otherColors, otherColors.blue)
+// #f00 { green: '#0f0', blue: '#00f' } #00f
