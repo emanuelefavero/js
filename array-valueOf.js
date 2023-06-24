e40c2533bd75964b31d774a058d5f50b924fdb5a
emanuelefavero <emafaux89@gmail.com>
2023-06-24 15:36:23 +0200

    add array-valueOf

---
+++ array-valueOf.js
@@ -0,0 +1,6 @@
+// The valueOf method returns the array itself
+let colors = ['red', 'green', 'blue']
+console.log(colors.valueOf()) // [ 'red', 'green', 'blue' ]
+console.log(colors) // [ 'red', 'green', 'blue' ]
+
+// TIP: The valueOf() method does not change the original array.
