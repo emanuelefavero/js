27c33b0251314f59d7aa5295a429a5f593ebf102
emanuelefavero <emafaux89@gmail.com>
2023-06-25 22:22:18 +0200

    add Boolean

---
+++ Boolean.js
@@ -0,0 +1,7 @@
+// The Boolean function checks if an expression is true
+
+console.log(Boolean(10 > 5)) // true
+console.log(Boolean(10 > 20)) // false
+
+// You can also omit the Boolean function and just check this way
+console.log(10 > 5) // true
