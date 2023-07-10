e737a3b106be07549f3297dadaea33b170a98dff
emanuelefavero <emafaux89@gmail.com>
2023-07-10 18:53:39 +0200

    add class-static

---
+++ class-static.js
@@ -0,0 +1,10 @@
+// The static method can be called without instantiating the class
+
+class MathUtils {
+  // static method
+  static add(a, b) {
+    return a + b
+  }
+}
+
+console.log(MathUtils.add(2, 2)) // 4
