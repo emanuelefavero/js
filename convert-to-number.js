e5800f583c7a71dd9645d7b7a3ff2ccfd89d6c31
emanuelefavero <emafaux89@gmail.com>
2023-07-13 17:00:00 +0200

    add log statements to convert-to-number, string

--- convert-to-number.js
+++ convert-to-number.js
@@ -1,2 +1,4 @@
 const stringNumber = '2'
 const number = Number(stringNumber)
+
+console.log(number) // 2
--- convert-to-string.js
+++ convert-to-string.js
@@ -1,3 +1,5 @@
 const number = 2
 const stringNumber = number + ''
 const stringNumber2 = number.toString()
+
+console.log(stringNumber) // 2
