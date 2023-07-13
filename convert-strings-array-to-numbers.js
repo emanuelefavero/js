3ebcede72a40da7c02aad5d7f1884d0c291d6bf2
emanuelefavero <emafaux89@gmail.com>
2023-07-13 17:02:15 +0200

    add comments to - convert-strings-array-to-numbers.js

--- convert-strings-array-to-numbers.js
+++ convert-strings-array-to-numbers.js
@@ -1,10 +1,11 @@
 // Convert string array to number array
-console.log(['1.25', '10', '101'].map(Number))
+console.log(['1.25', '10', '101'].map(Number)) // [ 1.25, 10, 101 ]
 
 // ----------------
 
 // Convert string array to integer array
 console.log(['1.25', '1.34234', '2.6'].map((item) => parseInt(item)))
+// [ 1, 1, 2 ]
 
 // ? * parseInt only works if passed this way to map. It has to do with the second argument of parseInt, which is the radix. If you don't pass it, it will default to 10. If you need to pass the radix, you need to pass it as the second argument of map, like this:
 // console.log(['1.25', '1.34234', '2.6'].map(parseInt, 10))
@@ -13,6 +14,7 @@ console.log(['1.25', '1.34234', '2.6'].map((item) => parseInt(item)))
 
 // Convert string array to float array
 console.log(['1.25', '1.34234', '2.6'].map(parseFloat))
+// [ 1.25, 1.34234, 2.6 ]
 
 // ----------------
 
@@ -24,3 +26,4 @@ console.log(
     // STRING => parseFloat => FLOAT => toFixed => STRING => parseFloat => FLOAT
   )
 )
+// [ 1.25, 1.34, 2.6 ]
