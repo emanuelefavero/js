49008c50ea3de1a2b438738497adde6df67088f6
emanuelefavero <emafaux89@gmail.com>
2023-07-19 12:58:18 +0200

    add for-of loop

---
+++ for-of.js
@@ -0,0 +1,10 @@
+let array = ['Hello', 'Hola', 'Ciao']
+
+// * We can also loop an array like this over classic for loops or forEach loops
+for (let element of array) {
+  console.log(element)
+}
+
+// Hello
+// Hola
+// Ciao
