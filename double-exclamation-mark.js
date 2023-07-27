d4c3f2042167e2b0926e72ebcc5d33968d7da3aa
emanuelefavero <emafaux89@gmail.com>
2023-07-28 00:15:26 +0200

    add double-excalamation-mark.js

---
+++ double-exclamation-mark.js
@@ -0,0 +1,6 @@
+// The !! double exclamation mark operator casts a value to a boolean
+// TIP: It is a shorthand for Boolean(value)
+let value1 = 0 // falsy value
+let value2 = 1 // truthy value
+console.log(!!value1) // false
+console.log(!!value2) // true
