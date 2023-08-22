c65cc9ff2b1a25bd0f73f030234494036bb812c4
emanuelefavero <emafaux89@gmail.com>
2023-08-22 05:04:14 +0200

    add array-two-dimensional

---
+++ array-two-dimensional.js
@@ -0,0 +1,19 @@
+// * The following code shows how to access the elements of a two-dimensional array
+let array = [
+  [1, 2, 3],
+  [4, 5, 6],
+]
+
+console.log(array[0][0]) // 1
+console.log(array[1][0]) // 4
+
+// * Get all the values of a two-dimensional array as a single-dimensional array
+let values = []
+
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array[i].length; j++) {
+    values.push(array[i][j])
+  }
+}
+
+console.log(values) // [1, 2, 3, 4, 5, 6]
