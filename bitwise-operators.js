63225f9f3c046895ea86f9e78972104315bbcd58
emanuelefavero <emafaux89@gmail.com>
2023-08-27 03:13:10 +0200

    add left, right shift to bitwise-operators

--- bitwise-operators.js
+++ bitwise-operators.js
@@ -26,3 +26,16 @@ console.log(3 & 4) // Bitwise AND, outputs 0 since 00000000 is 0 in decimal
 console.log(0 ^ 1) // Bitwise XOR, outputs 1 since 00000001 is 1 in decimal
 // ----------↓↓
 // 1 = 00000001
+
+// * Left Shift, Right Shift
+// Left Shift (<<) - Shifts the bits of the first operand left by the number of places specified in the second operand. Excess bits shifted off to the left are discarded. Zero bits are shifted in from the right
+
+// Shift 1 to the left by 2 places
+// 1 = 00000001
+console.log(1 << 2) // Outputs 4 since 00000100 is 4 in decimal
+
+// Right Shift (>>) - Shifts the first operand the specified number of bits to the right. Excess bits shifted off to the right are discarded. Copies of the leftmost bit are shifted in from the left. Since the new leftmost bit has the same value as the previous leftmost bit, the sign bit (the leftmost bit) does not change. Hence the name "sign-propagating"
+
+// Shift 4 to the right by 2 places
+// 4 = 00000100
+console.log(4 >> 2) // Outputs 1 since 00000001 is 1 in decimal
