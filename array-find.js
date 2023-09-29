0722c793aeb6bd37e4b40d7abc2e5f800733d8e0
emanuelefavero <emafaux89@gmail.com>
2023-09-29 19:05:41 +0200

    add comments to array-find

--- array-find.js
+++ array-find.js
@@ -1,4 +1,9 @@
 // ARRAY FIND - Returns the value of the first element in an array that pass a test
 let ages = [18, 21, 34]
+
 console.log(ages.find((age) => age >= 21)) // 21
+
 console.log(ages.find((age) => age === 18)) // 18
+
+// TIP: findIndex() works the same way, but returns the index of the first element that satisfies the condition
+// TIP: find() does not change the original array
