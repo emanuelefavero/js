110b3510e372c176473f75b0e1c9c061c78f2a4c
emanuelefavero <emafaux89@gmail.com>
2023-10-11 20:26:04 +0200

    refactor sort-no-mutation

--- sort-no-mutation.js
+++ sort-no-mutation.js
@@ -1,14 +1,18 @@
 // Sort the array without mutating the original array
-let nums = [4, 1, 3, 2]
+let nums = [1, 10, 21, 2]
 
-let sorted = nums.toSorted()
-console.log(sorted) // [1, 2, 3, 4]
+let sorted = nums.toSorted((a, b) => a - b)
+console.log(sorted) // [1, 2, 10, 21]
 
 sorted = nums.toSorted((a, b) => b - a) // reverse sort
-console.log(sorted) // [4, 3, 2, 1]
+console.log(sorted) // [21, 10, 2, 1]
 
 // TIP: You can also use the spread operator to create a copy of the array
 sorted = [...nums].sort((a, b) => a - b)
-console.log(sorted) // [1, 2, 3, 4]
+console.log(sorted) //
 
-console.log(nums) // [4, 1, 3, 2] - STILL THE SAME
+console.log(nums) // [1, 10, 21, 2] - STILL THE SAME
+
+// BEWARE: toSorted() without a callback function sorts the array lexicographically (alphabetically)
+sorted = nums.toSorted()
+console.log(sorted) // [1, 10, 2, 21] - NOT WHAT WE WANT
