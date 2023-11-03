f01deb452c7b35d9c90cea1ae139d3474c3a7a04
emanuelefavero <emafaux89@gmail.com>
2023-11-03 19:56:31 +0100

    add array-flat

---
+++ array-flat.js
@@ -0,0 +1,3 @@
+// * Array.flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+
+console.log([[1, 2], [3], 4].flat()) // [1, 2, 3, 4]
