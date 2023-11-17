f6de3c4e5ae5e2a0e21d8da0b2af647557a099de
emanuelefavero <emafaux89@gmail.com>
2023-11-17 23:40:58 +0100

    add the math object - math.js

---
+++ math.js
@@ -0,0 +1,16 @@
+// * The Math Object
+// The Math Object is a built-in object that has properties and methods for mathematical constants and functions
+
+// * Math Constants
+
+// Math.E returns Euler's number, the base of natural logarithms, e, approximately 2.718.
+console.log(Math.E) // 2.718281828459045
+
+// Math.PI returns the ratio of the circumference of a circle to its diameter, approximately 3.14159.
+console.log(Math.PI) // 3.141592653589793
+
+// Math.SQRT2 returns the square root of 2, approximately 1.414.
+console.log(Math.SQRT2) // 1.4142135623730951
+
+// Math.SQRT1_2 returns the square root of 1/2, approximately 0.707
+console.log(Math.SQRT1_2) // 0.7071067811865476
