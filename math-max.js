911e5eeec9c6620967bf1b2e16a36866b77d61db
emanuelefavero <emafaux89@gmail.com>
2023-11-17 12:32:41 +0100

    add math-max

---
+++ math-max.js
@@ -0,0 +1,5 @@
+// * Math.max() returns the largest of zero or more numbers.
+console.log(Math.max()) // -Infinity
+console.log(Math.max(1)) // 1
+console.log(Math.max(1, 3)) // 3
+console.log(Math.max(1, 3, 2)) // 3
