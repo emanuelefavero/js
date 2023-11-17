630d932574c4454a05a0857fdc1d10854810a642
emanuelefavero <emafaux89@gmail.com>
2023-11-17 23:42:57 +0100

    finish to add math object constants to math.js

--- math.js
+++ math.js
@@ -14,3 +14,15 @@ console.log(Math.SQRT2) // 1.4142135623730951
 
 // Math.SQRT1_2 returns the square root of 1/2, approximately 0.707
 console.log(Math.SQRT1_2) // 0.7071067811865476
+
+// Math.LN2 returns the natural logarithm of 2, approximately 0.693
+console.log(Math.LN2) // 0.6931471805599453
+
+// Math.LN10 returns the natural logarithm of 10, approximately 2.302
+console.log(Math.LN10) // 2.302585092994046
+
+// Math.LOG2E returns the base 2 logarithm of E, approximately 1.442 (E is Euler's number, the base of the natural logarithms)
+console.log(Math.LOG2E) // 1.4426950408889634
+
+// Math.LOG10E returns the base 10 logarithm of E, approximately 0.434 (E is Euler's number, the base of the natural logarithms)
+console.log(Math.LOG10E) // 0.4342944819032518
