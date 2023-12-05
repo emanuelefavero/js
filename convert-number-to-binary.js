e65874555db8f28c0962ed84ab653da3f21cdce4
emanuelefavero <emafaux89@gmail.com>
2023-12-05 06:12:01 +0100

    add convert-number-to-binary

---
+++ convert-number-to-binary.js
@@ -0,0 +1,12 @@
+// * Convert a number to binary
+let num = 2
+console.log(num.toString(2)) // 10
+
+num = 10
+console.log(num.toString(2)) // 1010
+
+// TIP: By passing 2 as the radix argument, we can convert a number to binary
+
+// * Convert binary to number
+let binaryNum = '1010'
+console.log(parseInt(binaryNum, 2)) // 10
