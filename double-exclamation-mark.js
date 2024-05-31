16d16f8eb459589c7f9d99b90b63c1c49d9b1273
emanuelefavero <emafaux89@gmail.com>
2024-05-31 22:07:21 +0200

    udpate double-exclamation-mark

--- double-exclamation-mark.js
+++ double-exclamation-mark.js
@@ -4,3 +4,7 @@ let value1 = 0 // falsy value
 let value2 = 1 // truthy value
 console.log(!!value1) // false
 console.log(!!value2) // true
+
+// TIP: It can also be used to transform any value to its corresponding boolean
+console.log(!!'hello') // true
+console.log(!!'') // false
